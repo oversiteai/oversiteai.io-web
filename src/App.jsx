7abb6717146b94241fc742729c0da3fae51abf3d
oversiteai <dev@oversiteai.io>
2025-07-30 10:30:29 -0500

    move to hashrouter for static hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header';
@@ -68,7 +68,7 @@ function AppContent() {
 
 function App() {
   return (
-    <Router basename="/oversiteai.io-web/">
+    <Router>
       <AppContent />
     </Router>
   );
